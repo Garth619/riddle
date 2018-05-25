a1e7c22e5afb1109d229735593a7e56acfc00cf1
Garrett Cullen <garrettwcullen@gmail.com>
2018-05-25 14:17:38 -0700

    push

--- wp-content/themes/riddle/js/custom-min.js
+++ wp-content/themes/riddle/js/custom-min.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function($){});
+jQuery(document).ready(function($){$("nav ul.menu > li.menu-item-has-children a").on("click",function(e){$(this).parents("li.menu-item-has-children").toggleClass("open"),$(this).next("ul.sub-menu").toggleClass("open")})});
--- wp-content/themes/riddle/js/custom.js
+++ wp-content/themes/riddle/js/custom.js
@@ -1,3 +1,15 @@
 jQuery(document).ready(function($){
+	
+	
+	$('nav ul.menu > li.menu-item-has-children a').on('click', function(e) {
+		
+			$(this).parents('li.menu-item-has-children').toggleClass('open');
+			
+			$(this).next('ul.sub-menu').toggleClass('open');
+	  
+	});
+	
+		
+	
   
 }); // document ready
