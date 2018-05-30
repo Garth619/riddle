e94f02ac5f971f99e144d00cd89eef050a87e2b1
Garrett Cullen <garrettwcullen@gmail.com>
2018-05-30 16:10:21 -0700

    push

---
+++ wp-content/themes/riddle/homepage_template_parts/section-2.php
@@ -0,0 +1,74 @@
+<section id="section_two">
+	
+	<div class="sec_two_title_wrapper">
+		
+		<span class="sec_two_title">Why Choose Our Firm</span><!-- sec_two_title -->
+		
+	</div><!-- sec_two_title_wrapper -->
+	
+	
+	<div class="inner_sec_two">
+		
+	
+	
+	<div class="sec_two_slider">
+			
+			<div class="sec_two_single_slide one">
+				
+				
+				<div class="sp_1_wrapper">
+					
+					
+					<?php echo file_get_contents("wp-content/themes/riddle/images/new_sp_1.svg"); ?>
+						
+					
+				</div><!-- sp_1_wrapper -->
+				
+				
+			</div><!-- sec_two_single_slide -->
+			
+			
+			<div class="sec_two_single_slide one">
+				
+				
+				<div class="sp_1_wrapper">
+					
+					
+					<?php echo file_get_contents("wp-content/themes/riddle/images/new_sp_1.svg"); ?>
+						
+					
+				</div><!-- sp_1_wrapper -->
+				
+				
+			</div><!-- sec_two_single_slide -->
+			
+			
+			<div class="sec_two_single_slide one">
+				
+				
+				<div class="sp_1_wrapper">
+					
+					
+					<?php echo file_get_contents("wp-content/themes/riddle/images/new_sp_1.svg"); ?>
+						
+					
+				</div><!-- sp_1_wrapper -->
+				
+				
+			</div><!-- sec_two_single_slide -->
+			
+			
+			
+			
+			
+
+			
+			
+		</div><!-- sec_two_slider -->
+
+		
+		
+	</div><!-- inner_sec_two -->
+	
+	
+</section><!-- section_two -->
