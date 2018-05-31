3fce95fc80d88d1a2f0e4be1fffe84c1a84f48c2
Garrett Cullen <garrettwcullen@gmail.com>
2018-05-31 09:17:12 -0700

    push

--- wp-content/themes/riddle/homepage_template_parts/section-2.php
+++ wp-content/themes/riddle/homepage_template_parts/section-2.php
@@ -13,56 +13,69 @@
 	
 	<div class="sec_two_slider">
 			
-			<div class="sec_two_single_slide one">
+			<div class="sec_two_single_slide">
 				
 				
-				<div class="sp_1_wrapper">
+				<div class="sp_wrapper">
 					
 					
 					<?php echo file_get_contents("wp-content/themes/riddle/images/new_sp_1.svg"); ?>
+					
+					<span class="slide_title">over <strong>160 years</strong> of combined experience</span><!-- slide_title -->
+				
+				
+					<a class="view_button" href="">View Case Results</a><!-- view_button -->
 						
 					
-				</div><!-- sp_1_wrapper -->
+				</div><!-- sp_wrapper -->
+				
+				
+				
 				
 				
 			</div><!-- sec_two_single_slide -->
 			
 			
-			<div class="sec_two_single_slide one">
+			<div class="sec_two_single_slide">
 				
 				
-				<div class="sp_1_wrapper">
+				<div class="sp_wrapper">
 					
 					
-					<?php echo file_get_contents("wp-content/themes/riddle/images/new_sp_1.svg"); ?>
-						
+					<?php echo file_get_contents("wp-content/themes/riddle/images/new_sp_2.svg"); ?>
 					
-				</div><!-- sp_1_wrapper -->
+					
+					<span class="slide_title">we take calls <strong>24 hours a day, 7 days</strong> a week</span><!-- slide_title -->
+					
+					<a class="view_button" href="">View Case Results</a><!-- view_button -->
+					
+					
+			</div><!-- sp_wrapper -->
 				
 				
 			</div><!-- sec_two_single_slide -->
 			
 			
-			<div class="sec_two_single_slide one">
+			<div class="sec_two_single_slide">
 				
 				
-				<div class="sp_1_wrapper">
+				<div class="sp_wrapper">
 					
 					
-					<?php echo file_get_contents("wp-content/themes/riddle/images/new_sp_1.svg"); ?>
+					<?php echo file_get_contents("wp-content/themes/riddle/images/new_sp_3.svg"); ?>
+					
+					
+					<span class="slide_title">we have recovered <strong>millions</strong> for our clients</span><!-- slide_title -->
+					
+					<a class="view_button" href="">View Case Results</a><!-- view_button -->
 						
 					
-				</div><!-- sp_1_wrapper -->
+				</div><!-- sp_wrapper -->
 				
 				
 			</div><!-- sec_two_single_slide -->
-			
-			
-			
-			
 			
 
-			
 			
 		</div><!-- sec_two_slider -->
 
