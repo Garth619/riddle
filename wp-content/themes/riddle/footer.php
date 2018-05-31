f7f4dc6dead6865416fffdcd34294db0a7ad3ae9
Garrett Cullen <garrettwcullen@gmail.com>
2018-05-31 10:58:25 -0700

    push

--- wp-content/themes/riddle/footer.php
+++ wp-content/themes/riddle/footer.php
@@ -1,3 +1,16 @@
+<footer>
+	
+	<div class="inner_footer">
+		
+		<span class="footer_title">Free<span></span> Consultation</span><!-- footer_title -->
+		
+		<span class="small_footer_title">no obligation or cost to you</span><!-- small_footer_title -->
+		
+	</div><!-- inner_footer -->
+	
+</footer>
+
+
 
 
 <?php wp_footer();?>
