c720c4279c910490d4bfb62964567e599189a880
Garrett Cullen <garrettwcullen@gmail.com>
2018-05-31 15:38:26 -0700

    push

--- wp-content/themes/riddle/footer.php
+++ wp-content/themes/riddle/footer.php
@@ -16,9 +16,104 @@
 		
 	</div><!-- inner_footer -->
 	
+	<div class="footer_locations">
+		
+		
+		<div class="locations_left">
+			
+			
+			<span class="large_header">Contact</span><!-- large_header -->
+			
+			<span class="small_header">Our Firm <strong>Today</strong></span><!-- small_header -->
+			
+			
+		</div><!-- locations_left -->
+		
+		
+		<div class="locations_right">
+			
+			
+			<div class="location_slider">
+				
+				<div class="single_location_slide">
+					
+					<span class="location_title">GoldsBoro</span><!-- location_title -->
+					
+					
+					<span class="address">601 North Spence Avenue<br/> Goldsboro, NC 27534</span><!-- address -->
+					
+					<a class="get_directions_button" href="">Get Directions</a><!-- get_directions_button -->
+					
+					<br/>
+					
+					<a class="tel" href="">(800) 525-7111</a>
+					
+				</div><!-- single_location_slide -->
+				
+				<div class="location_slider">
+				
+				<div class="single_location_slide">
+					
+					<span class="location_title">GoldsBoro</span><!-- location_title -->
+					
+					
+					<span class="address">601 North Spence Avenue<br/> Goldsboro, NC 27534</span><!-- address -->
+					
+					<a class="get_directions_button" href="">Get Directions</a><!-- get_directions_button -->
+					
+					<br/>
+					
+					<a class="tel" href="">(800) 525-7111</a>
+					
+				</div><!-- single_location_slide -->
+				
+				<div class="location_slider">
+				
+				<div class="single_location_slide">
+					
+					<span class="location_title">GoldsBoro</span><!-- location_title -->
+					
+					
+					<span class="address">601 North Spence Avenue<br/> Goldsboro, NC 27534</span><!-- address -->
+					
+					<a class="get_directions_button" href="">Get Directions</a><!-- get_directions_button -->
+					
+					<br/>
+					
+					<a class="tel" href="">(800) 525-7111</a>
+					
+				</div><!-- single_location_slide -->
+				
+				<div class="location_slider">
+				
+				<div class="single_location_slide">
+					
+					<span class="location_title">GoldsBoro</span><!-- location_title -->
+					
+					
+					<span class="address">601 North Spence Avenue<br/> Goldsboro, NC 27534</span><!-- address -->
+					
+					<a class="get_directions_button" href="">Get Directions</a><!-- get_directions_button -->
+					
+					<br/>
+					
+					<a class="tel" href="">(800) 525-7111</a>
+					
+				</div><!-- single_location_slide -->
+				
+			</div><!-- location_slider -->
+			
+			
+		</div><!-- locations_right -->
+		
+		
+		
+	</div><!-- footer_locations -->
+	
 </footer>
 
 <?php wp_footer();?>
+
 
 </body>
 </html>
