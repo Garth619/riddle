7a1e583156f802cd9b01df43dda2c7b5bc3d763a
Garrett Cullen <garrettwcullen@gmail.com>
2018-05-31 11:47:30 -0700

    push

--- wp-content/themes/riddle/footer.php
+++ wp-content/themes/riddle/footer.php
@@ -6,15 +6,17 @@
 		
 		<span class="small_footer_title">no obligation or cost to you</span><!-- small_footer_title -->
 		
+		<div class="form_wrapper">
+			
+			<?php gravity_form(1, false, false, false, '', true, 12); ?>
+
+		</div><!-- form_wrapper -->
+		
 	</div><!-- inner_footer -->
 	
 </footer>
 
-
-
-
 <?php wp_footer();?>
-
 
 </body>
 </html>
