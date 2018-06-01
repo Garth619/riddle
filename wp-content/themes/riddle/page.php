d21964b6a8bfea31f71b99e96df0e9b5128aa07d
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-01 15:28:03 -0700

    push

--- wp-content/themes/riddle/page.php
+++ wp-content/themes/riddle/page.php
@@ -1,13 +1,85 @@
 <?php get_header(); ?>
 
 
+<div class="main two_col">
 
-<?php get_template_part( 'loop', 'index' );?>
-
-<?php //use get_template_part( 'loop', 'page' ) if needed ?>
-
+	<div class="container">
 	
+	
+		<div class="inner_banner">
+		
+		
+		
+		</div><!-- inner_banner -->
+		
+		
+		<div class="inner_container content">
+			
+			<h1 class="pa_title">PRACTICE AREA LINE ONE GOES HERE AND THERE IS A SECOND</h1>
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
 
-<?php // get_sidebar(); ?>
+<h2>H2 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</h2>
+
+<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas <a href="">embedded link </a> aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+
+<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</blockquote>
+
+<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur:</p>
+
+<ul>
+	<li>Excepteur sint occ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.aecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+	<li>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</li>
+	<li>am rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</li> 
+</ul>
+<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p> 
+
+<h3>H3 Lorem ipsum dolor sit amet, consectetur ad</h3>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			
+			
+		</div><!-- inner_container -->
+	
+	
+	</div><!-- container -->
+
+
+	<div class="sidebar_wrapper">
+	
+	
+		<div class="sidebar_form">
+			
+			
+			<?php echo file_get_contents("wp-content/themes/riddle/images/new_diamonds.svg"); ?>
+			
+			
+			<div class="form_titles">
+				
+				<span class="large_header">Free Consultation</span><!-- large_header -->
+				
+				<span class="small_header">No Obligation or Cost to You</span><!-- small_header -->
+				
+			</div><!-- form_titles -->
+			
+			<?php gravity_form(2, false, false, false, '', true, 12); ?>
+			
+			
+		</div><!-- sidebar_form -->
+	
+	
+	</div><!-- sidebar_wrapper -->
+
+
+</div><!-- main -->
+
+
 
 <?php get_footer(); ?>
+
+
+
+<?php // get_template_part( 'loop', 'page' );?>
+
+	
