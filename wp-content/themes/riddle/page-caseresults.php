37c106e3bb59efe9a7f4e267dbe741b8d6c2d2a3
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-05 15:54:28 -0700

    push

---
+++ wp-content/themes/riddle/page-caseresults.php
@@ -0,0 +1,46 @@
+<?php
+	
+	/* Template Name: Case Results */
+	
+	get_header(); ?>
+
+
+<div class="main">
+
+	<div class="interior_banner">
+		
+		<h1 class="inetrior_banner_header">Case <span>Results</span></h1><!-- inetrior_banner_header -->
+		
+	</div><!-- interior_banner -->
+	
+	<div class="case_results_wrapper">
+			
+			
+			<div class="single_case_results">
+				
+				<a class="" href="">
+				
+					<span class="amount">$7.85m</span><!-- amount -->
+				
+					<span class="case_type">chemical spill&nbsp;&nbsp;|&nbsp;&nbsp;premises liability</span><!-- case_type -->
+					
+					<span class="case_results_read_more">Read Case Results</span><!-- case_results_read_more -->
+				
+				</a>
+				
+			</div><!-- single_case_results -->
+			
+			
+		</div><!-- case_results_wrapper -->
+
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
+
+
+
+
+
+	
