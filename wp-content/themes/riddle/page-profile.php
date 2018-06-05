450dc0c5f8d8293a29e07e1330d8e172d9fa51bc
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-05 11:31:28 -0700

    push

---
+++ wp-content/themes/riddle/page-profile.php
@@ -0,0 +1,77 @@
+<?php
+	
+	/* Template Name: Att Bio */
+	
+	get_header(); ?>
+
+
+<div class="main">
+	
+	<div class="profile_wrapper content">
+		
+		
+		<div class="profile_left">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/att.jpg"/>
+			
+		</div><!-- profile_left -->
+	
+		<div class="profile_right">
+			
+			<h1 class="attorney_title">Gene <span class="last_name">Riddle</span></h1><!-- attorney_title -->
+			
+			<span class="att_position">Personal Injury Lawyer & Stanly County Native  |  Founder</span><!-- att_position -->
+			
+			<div class="att_profile_content">
+			
+			<p>Gene Riddle is a founding member of Riddle & Brantley, LLP, a law firm that employs 10 lawyers with more than 160 years of combined legal experience. Gene is quick to stress his family’s deep North Carolina ties. A Stanly County native, he grew up in Aberdeen after moving from Albemarle. He has practiced law in North Carolina since September 1985. Gene’s parents were both educators. His father, a longtime superintendent of Moore County Schools, encouraged him to go to law school after college.</p>
+
+
+<blockquote>“My dad is probably the most honest person that was ever born,” he says. “Honest to a fault. He said we needed more honest lawyers, and I’d be a good one.”</blockquote>
+
+
+<p>Today, Gene believes honesty, integrity and willingness to work hard are the bedrock principles of a good attorney. That is what he and the other lawyers at Riddle & Brantley, LLP provide to their injured clients. Gene spends much of his time reviewing cases as well as instructing and supervising attorneys and paralegals. However, he also continues to directly handle cases as the lead attorney and he touches most of the personal injury cases that come into the firm.</p>
+
+<h2>North Carolina Personal Injury Lawyer Committed to 24/7 Client Service</h2>
+<p>Gene is particularly proud of his firm’s pledge to respond to phone calls and e-mails 24/7. As a result, he monitors almost all inquiries received, even after hours and on weekends. Gene takes on cases that involve serious personal injury in car accidents, truck accidents, dog bites, premises liability, wrongful death and violations of Constitutional Rights. In every case, Gene’s objective is obtaining an outcome that his client believes is fair.</p>
+
+<h2>Attorney with a Reputation for Success in State and Federal Courtrooms</h2>
+<p>Justice usually means compensation for the injured party’s losses, including their pain and suffering. Still, according to Gene, in many cases the acknowledgement of the defendant’s fault or even an apology is also valuable.</p>
+
+<p>In one case, Gene represented a spectator at a high school basketball game who was badly injured when bleachers he was climbing shifted. The client fell through, severely injuring his leg. The man suffered permanent injury to his leg and incurred $2,500 in medical bills. The bleachers were not pulled out properly and stabilized, leading to the accident, Gene says. However, the school system would not admit any negligence, nor would it consider any settlement. This negligence prompted a courtroom battle where Gene prevailed. In Carter v. Wayne County Board of Education, the jury returned a verdict of $250,000 for the plaintiff.</p>
+
+<p>Gene also represented five women in a claim against a Wayne County sheriff’s deputy. These women alleged the deputy had demanded sex in return for favorable treatment for their pending criminal cases. Four of Gene’s clients settled their lawsuits against the Wayne County Sheriff’s Department and the county. One client proceeded to federal court against the deputy, alleging he had violated her Constitutional protection against cruel and unusual punishment. Gene prevailed for his client in the courtroom.</p>
+
+<p>In Fordman v. Braswell, the jury awarded the plaintiff $1.5 million in actual damages and $3.5 million in punitive damages. According to news reports, after the court heard the evidence Gene presented on behalf of the plaintiff, the defense said it would no longer contest the case. Judge Terrence Boyle then dismissed the jury and imposed the judgment.</p>
+
+<h2>Goldsboro Wrongful Death Case: “I Believe I Truly Got Justice”</h2>
+<p>On his desk, Gene keeps a photo of 16-year-old girl, her parents’ only child. The girl died after a tractor-trailer hit her car in an intersection in Goldsboro. The initial investigation of the accident supported the truck driver’s version of the facts. He said he had a green light at the time of the accident. The girl’s devastated parents were not satisfied with the truck driver’s account, so they contacted Riddle & Brantley, LLP. Our investigation revealed a different story.</p>
+
+<p>Investigators tracked down eyewitnesses who contradicted the truck driver. Gene then hired accident reconstructionists whose forensic work showed that the truck driver had indeed run a red light. The trucking company’s insurers agreed with the new evidence and settled, paying the grieving parents $840,000. Additionally, the truck driver faced charges for misdemeanor death by motor vehicle. He pled guilty and eventually lost his commercial driver’s license for a year.</p>
+
+<h2>Memberships and Professional Recognition</h2>
+<p>Gene’s legal accomplishments have earned him several professional honors from his legal peers, including the 2014 Litigator Award. He also has a high AV Rating from Martindale-Hubbell as well as designation among the Best Attorneys of America and the American Trial Lawyers Association’s Top 100 Trial Lawyers. Additionally, he earned membership in the Million Dollar and Multi-Million Dollar Advocates Forums, based on verdicts won and settlements reached.</p>
+
+<p>However, the award Gene treasures most is the Old North State Award, which he received in 2009. The Old North State Award is presented by the North Carolina governor to individuals who have provided exemplary service and commitment to the state. Gene received it for his community service as a member and chairman of the Wayne County Board of Elections over four years.</p>
+
+<p>Why does the Old North State Award mean so much to Gene? It is the state’s second-highest award. The state’s highest civilian award is induction by the governor into the Order of the Longleaf Pine in recognition of service to the state. Gene’s father received an Order of the Longleaf Pine Award upon his retirement in 1999.</p>
+
+<h2>Family and Free Time</h2>
+<p>Gene and his wife have two daughters and several pets, including a Westie named “Coconut” who has been featured in several TV commercials. Church activities are also an important part of their lives. In his free time, Gene enjoys several sports, including tennis, golf and skiing.</p>
+
+			</div><!-- profile_content -->
+			
+			
+		</div><!-- profile_right -->
+		
+		
+	</div><!-- profile_wrapper -->
+	
+	
+</div><!-- main -->
+
+<?php get_footer(); ?>
+
+<?php // get_template_part( 'loop', 'page' );?>
+
+	
--- wp-content/themes/riddle/page.php
+++ wp-content/themes/riddle/page.php
@@ -15,32 +15,9 @@
 		
 		<div class="inner_container content">
 			
-			<h1 class="pa_title">PRACTICE AREA LINE ONE GOES HERE AND THERE IS A SECOND</h1>
+			<h1 class="pa_title"><?php the_title();?></h1>
 			
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-			
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-
-<h2>H2 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</h2>
-
-<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas <a href="">embedded link </a> aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
-
-
-<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</blockquote>
-
-<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur:</p>
-
-<ul>
-	<li>Excepteur sint occ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.aecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
-	<li>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</li>
-	<li>am rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</li> 
-</ul>
-<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p> 
-
-<h3>H3 Lorem ipsum dolor sit amet, consectetur ad</h3>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			
+			<?php get_template_part( 'loop', 'page' );?>
 			
 		</div><!-- inner_container -->
 	
@@ -93,6 +70,6 @@
 
 
 
-<?php // get_template_part( 'loop', 'page' );?>
+
 
 	
