786b9cb93e56c65b3c4bede213d48a4267d10388
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-06 13:50:30 -0700

    push

--- wp-content/themes/riddle/page-videocenter.php
+++ wp-content/themes/riddle/page-videocenter.php
@@ -18,12 +18,110 @@
 		
 		<div class="single_video">
 			
+			<div class="play">
+				
+				<?php echo file_get_contents("wp-content/themes/riddle/images/new_play.svg"); ?>
+				
+			</div><!-- play -->
 			
+			<div class="wistia_video_overlay">
+				
+				
+			</div><!-- wistia_video_overlay -->
+			
+			<div class="my_wistia wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- wistia -->
+			
+			<div class="video_center_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Video title Goes here Goes hereGoes here two lines</div><!-- video_center_title -->
 			
 		</div><!-- single_video -->
 		
 		
+		<div class="single_video">
+			
+			<div class="play">
+				
+				<?php echo file_get_contents("wp-content/themes/riddle/images/new_play.svg"); ?>
+				
+			</div><!-- play -->
+			
+			<div class="wistia_video_overlay">
+				
+				
+			</div><!-- wistia_video_overlay -->
+			
+			<div class="my_wistia wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- wistia -->
+			
+			<div class="video_center_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Video title Goes here Goes hereGoes here two lines</div><!-- video_center_title -->
+			
+		</div><!-- single_video -->
+		
+		
+		<div class="single_video">
+			
+			<div class="play">
+				
+				<?php echo file_get_contents("wp-content/themes/riddle/images/new_play.svg"); ?>
+				
+			</div><!-- play -->
+			
+			<div class="wistia_video_overlay">
+				
+				
+			</div><!-- wistia_video_overlay -->
+			
+			<div class="my_wistia wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- wistia -->
+			
+			<div class="video_center_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Video title Goes here Goes hereGoes here two lines</div><!-- video_center_title -->
+			
+		</div><!-- single_video -->
+		
+		
+		<div class="single_video">
+			
+			<div class="play">
+				
+				<?php echo file_get_contents("wp-content/themes/riddle/images/new_play.svg"); ?>
+				
+			</div><!-- play -->
+			
+			<div class="wistia_video_overlay">
+				
+				
+			</div><!-- wistia_video_overlay -->
+			
+			<div class="my_wistia wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- wistia -->
+			
+			<div class="video_center_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Video title Goes here Goes hereGoes here two lines</div><!-- video_center_title -->
+			
+		</div><!-- single_video -->
+		
+		
+		<div class="single_video">
+			
+			<div class="play">
+				
+				<?php echo file_get_contents("wp-content/themes/riddle/images/new_play.svg"); ?>
+				
+			</div><!-- play -->
+			
+			<div class="wistia_video_overlay">
+				
+				
+			</div><!-- wistia_video_overlay -->
+			
+			<div class="my_wistia wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- wistia -->
+			
+			<div class="video_center_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Video title Goes here Goes hereGoes here two lines</div><!-- video_center_title -->
+			
+		</div><!-- single_video -->
+		
+
+		
 	</div><!-- video_center -->
+	
+	<script src="https://fast.wistia.com/embed/medias/waichudc62.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async>
+			
+		</script>
 
 </div><!-- main -->
 
