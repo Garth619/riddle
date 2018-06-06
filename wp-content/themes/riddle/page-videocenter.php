f7887448a510188576a924af578be1aad3bf2546
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-06 11:04:55 -0700

    push

---
+++ wp-content/themes/riddle/page-videocenter.php
@@ -0,0 +1,38 @@
+<?php
+	
+	/* Template Name: Video Center */
+	
+	get_header(); ?>
+
+
+<div class="main">
+
+	<div class="interior_banner">
+		
+		<h1 class="inetrior_banner_header"><?php the_field( 'first_word' ); ?> <span><?php the_field( 'second_word' ); ?></span></h1><!-- inetrior_banner_header -->
+		
+	</div><!-- interior_banner -->
+	
+	<div class="video_center">
+		
+		
+		<div class="single_video">
+			
+			
+			
+		</div><!-- single_video -->
+		
+		
+	</div><!-- video_center -->
+
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
+
+
+
+
+
+	
