f9e4031d450370939572b93b77495853878e1a7d
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-07 09:52:38 -0700

    push

---
+++ wp-content/themes/riddle/page-contact.php
@@ -0,0 +1,65 @@
+<?php
+	
+	/* Template Name: Contact */
+	
+	get_header(); ?>
+
+
+<div class="main">
+
+
+	
+	<div class="contact_wrapper">
+		
+		
+		<div class="contact_left content">
+			
+			<h1 class="contact_title">Contact <span>Us</span></h1><!-- contact_title -->
+			
+			<div class="left_content">
+				
+				<h2>Are you in need of a personal injury lawyer or car accident attorney?</h2>
+			
+				<p>Perhaps you need someone to assist you with a workers’ compensation or Social Security Disability case. If so, please use the form below to contact us at Riddle & Brantley. Our experienced North Carolina Injury attorneys can provide you with a free consultation where we can personally discuss the details of your personal injury case with you. We look forward to speaking with you! With more than 130 years of combined legal experience, the team of attorneys at Riddle & Brantley, LLP can help to provide the experience to guide you through the entire personal injury litigation process and the determination to fight for your injury compensation. Our main office is located in Goldsboro and we proudly serve injury victims throughout the state of North Carolina, including:</p>
+				
+			
+					<ul>
+						
+						<li>Goldsboro</li>
+						<li>Raleigh</li>
+						<li>Jacksonville</li>
+						<li>Kinston</li>
+						<li>Most of the towns and suburbs that surround these areas.</li>
+						
+					</ul>
+					
+				
+					<p>To learn more about your legal options, contact us today at <a href="tel:1-800-525-7111">1-800-525-7111.</a> We have intake staff that are ready to take your call and after some initial questions about the nature of your inquiry, you will be quickly connected to one of our highly skilled attorneys. If you would prefer to send your questions electronically, simply fill out the free case evaluation form located at the top of this page. It’s strictly confidential and 100% FREE to submit.</p>
+			
+			</div><!-- left_content -->
+
+			
+		</div><!-- contact_left -->
+		
+		<div class="contact_right">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/intl_contact_img.jpg"/>
+			
+		</div><!-- contact_right -->
+		
+		
+	</div><!-- contact_wrapper -->
+	
+	
+
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
+
+
+
+
+
+	
