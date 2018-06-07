58d3fc38498419444736da9949b4cd651326aded
Garrett Cullen <Garth619@users.noreply.github.com>
2018-06-06 21:34:12 -0700

    push

--- wp-content/themes/riddle/page-attdirectory.php
+++ wp-content/themes/riddle/page-attdirectory.php
@@ -17,150 +17,84 @@
 	<div class="attorney_directory">
 		
 		
-		<div class="single_att">
-			
-			<a class="" href="">
-			
-				<div class="single_att_image_wrapper">
-				
-					<img src="<?php bloginfo('template_directory');?>/images/att.jpg"/>
-					
-					<div class="singe_att_ovelay">
-						
-						<span class="view">View <span>Profile</span></span><!-- singe_att_ovelay -->
-						
-					</div><!-- singe_att_ovelay -->
-				
-				</div><!-- single_att_image_wrapper -->
-				
-				<span class="att_name">Gene Riddle</span><!-- att_name -->
-				
-				<span class="att_title">Personal Injury Lawyer</span><!-- att_title -->
-			
-			</a>
-			
-		</div><!-- single_att -->
+		<?php $posts = get_field('attorney_directory');
 		
-		<div class="single_att coming_soon">
+		if( $posts ): ?>
+		   
+		    <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+		        <?php setup_postdata($post); ?>
+		        
+		        
+		        <?php if(get_field( 'attorney_image' )):?>
+		        
+		        
+		        	<div class="single_att">
 			
-			<a class="" href="">
+								<a class="" href="<?php the_permalink();?>">
 			
-				<div class="single_att_image_wrapper">
+									<div class="single_att_image_wrapper">
 				
-					<img src="<?php bloginfo('template_directory');?>/images/placeholder.png"/>
+										<?php $attorney_image = get_field( 'attorney_image' ); ?>
+										
+										<img src="<?php echo $attorney_image['url']; ?>" alt="<?php echo $attorney_image['alt']; ?>" />
+										
+										<div class="singe_att_ovelay">
+						
+											<span class="view">View <span>Profile</span></span><!-- singe_att_ovelay -->
+						
+										</div><!-- singe_att_ovelay -->
+				
+									</div><!-- single_att_image_wrapper -->
+				
+									<span class="att_name"><?php the_title();?></span><!-- att_name -->
+				
+									<span class="att_title"><?php the_field( 'attorney_position' ); ?></span><!-- att_title -->
+			
+							</a>
+			
+						</div><!-- single_att -->
+						
+						
+						<?php else:?>
+						
+						
+						
+						<div class="single_att coming_soon">
+			
+							<a class="" href="<?php the_permalink();?>">
+			
+								<div class="single_att_image_wrapper">
+				
+									<img src="<?php bloginfo('template_directory');?>/images/placeholder.png"/>
 					
-					<div class="singe_att_ovelay">
+									<div class="singe_att_ovelay">
 						
-						<span class="no_photo">Photo Coming Soon</span><!-- no_photo -->
+										<span class="no_photo">Photo Coming Soon</span><!-- no_photo -->
 						
-					</div><!-- singe_att_ovelay -->
+									</div><!-- singe_att_ovelay -->
 				
-				</div><!-- single_att_image_wrapper -->
+								</div><!-- single_att_image_wrapper -->
 				
-				<span class="att_name">Gene Riddle</span><!-- att_name -->
+								<span class="att_name"><?php the_title();?></span><!-- att_name -->
 				
-				<span class="att_title">Personal Injury Lawyer</span><!-- att_title -->
+								<span class="att_title"><?php the_field( 'attorney_position' ); ?></span><!-- att_title -->
 			
-			</a>
+							</a>
 			
-		</div><!-- single_att -->
-		
-		<div class="single_att">
-			
-			<a class="" href="">
-			
-				<div class="single_att_image_wrapper">
-				
-					<img src="<?php bloginfo('template_directory');?>/images/att.jpg"/>
+						</div><!-- single_att -->
 					
-					<div class="singe_att_ovelay">
-						
-						<span class="view">View <span>Profile</span></span><!-- singe_att_ovelay -->
-						
-					</div><!-- singe_att_ovelay -->
-				
-				</div><!-- single_att_image_wrapper -->
-				
-				<span class="att_name">Gene Riddle</span><!-- att_name -->
-				
-				<span class="att_title">Personal Injury Lawyer</span><!-- att_title -->
-			
-			</a>
-			
-		</div><!-- single_att -->
-		
-		<div class="single_att">
-			
-			<a class="" href="">
-			
-				<div class="single_att_image_wrapper">
-				
-					<img src="<?php bloginfo('template_directory');?>/images/att.jpg"/>
-					
-					<div class="singe_att_ovelay">
-						
-						<span class="view">View <span>Profile</span></span><!-- singe_att_ovelay -->
-						
-					</div><!-- singe_att_ovelay -->
-				
-				</div><!-- single_att_image_wrapper -->
-				
-				<span class="att_name">Gene Riddle</span><!-- att_name -->
-				
-				<span class="att_title">Personal Injury Lawyer</span><!-- att_title -->
-			
-			</a>
-			
-		</div><!-- single_att -->
-		
-		<div class="single_att">
-			
-			<a class="" href="">
-			
-				<div class="single_att_image_wrapper">
-				
-					<img src="<?php bloginfo('template_directory');?>/images/att.jpg"/>
-					
-					<div class="singe_att_ovelay">
-						
-						<span class="view">View <span>Profile</span></span><!-- singe_att_ovelay -->
-						
-					</div><!-- singe_att_ovelay -->
-				
-				</div><!-- single_att_image_wrapper -->
-				
-				<span class="att_name">Gene Riddle</span><!-- att_name -->
-				
-				<span class="att_title">Personal Injury Lawyer</span><!-- att_title -->
-			
-			</a>
-			
-		</div><!-- single_att -->
-		
-		<div class="single_att">
-			
-			<a class="" href="">
-			
-				<div class="single_att_image_wrapper">
-				
-					<img src="<?php bloginfo('template_directory');?>/images/att.jpg"/>
-					
-					<div class="singe_att_ovelay">
-						
-						<span class="view">View <span>Profile</span></span><!-- singe_att_ovelay -->
-						
-					</div><!-- singe_att_ovelay -->
-				
-				</div><!-- single_att_image_wrapper -->
-				
-				<span class="att_name">Gene Riddle</span><!-- att_name -->
-				
-				<span class="att_title">Personal Injury Lawyer</span><!-- att_title -->
-			
-			</a>
-			
-		</div><!-- single_att -->
-		
+
+		        
+		        <?php endif;?>
+		        
+		        
+		    <?php endforeach; ?>
+		   
+		   
+		   
+		   
+		    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+		<?php endif; ?>		
 				
 	</div><!-- attorney_directory -->
 	
