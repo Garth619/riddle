203e2065b793e81fc3ee926fd715bede9689007d
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-07 15:17:31 -0700

    push

--- wp-content/themes/riddle/page-contact.php
+++ wp-content/themes/riddle/page-contact.php
@@ -14,27 +14,11 @@
 		
 		<div class="contact_left content">
 			
-			<h1 class="contact_title">Contact <span>Us</span></h1><!-- contact_title -->
+			<h1 class="contact_title"><?php the_field( 'first_word' ); ?> <span><?php the_field( 'second_word' ); ?></span></h1><!-- contact_title -->
 			
 			<div class="left_content">
 				
-				<h2>Are you in need of a personal injury lawyer or car accident attorney?</h2>
-			
-				<p>Perhaps you need someone to assist you with a workers’ compensation or Social Security Disability case. If so, please use the form below to contact us at Riddle & Brantley. Our experienced North Carolina Injury attorneys can provide you with a free consultation where we can personally discuss the details of your personal injury case with you. We look forward to speaking with you! With more than 130 years of combined legal experience, the team of attorneys at Riddle & Brantley, LLP can help to provide the experience to guide you through the entire personal injury litigation process and the determination to fight for your injury compensation. Our main office is located in Goldsboro and we proudly serve injury victims throughout the state of North Carolina, including:</p>
-				
-			
-					<ul>
-						
-						<li>Goldsboro</li>
-						<li>Raleigh</li>
-						<li>Jacksonville</li>
-						<li>Kinston</li>
-						<li>Most of the towns and suburbs that surround these areas.</li>
-						
-					</ul>
-					
-				
-					<p>To learn more about your legal options, contact us today at <a href="tel:1-800-525-7111">1-800-525-7111.</a> We have intake staff that are ready to take your call and after some initial questions about the nature of your inquiry, you will be quickly connected to one of our highly skilled attorneys. If you would prefer to send your questions electronically, simply fill out the free case evaluation form located at the top of this page. It’s strictly confidential and 100% FREE to submit.</p>
+				<?php the_field( 'contact_us_content' ); ?>
 			
 			</div><!-- left_content -->
 
@@ -43,7 +27,13 @@
 		
 		<div class="contact_right">
 			
-			<img src="<?php bloginfo('template_directory');?>/images/intl_contact_img.jpg"/>
+			<?php $contact_us_image = get_field( 'contact_us_image' ); ?>
+			
+			<?php if ( $contact_us_image ) { ?>
+			
+				<img src="<?php echo $contact_us_image['url']; ?>" alt="<?php echo $contact_us_image['alt']; ?>" />
+
+			<?php } ?>
 			
 		</div><!-- contact_right -->
 		
@@ -54,151 +44,50 @@
 	<div class="contact_list_wrapper">
 		
 		
-		<div class="single_contact_list">
+		
+		<?php if(get_field('city_locations')): ?>
+		 
+			<?php while(has_sub_field('city_locations')): ?>
 			
 			
-			<div class="iframe_map">
-				
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3253.0610585077893!2d-77.94924298474942!3d35.37894158026594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89ac01ebc7c80817%3A0x744230c84f6e2c87!2sRiddle+%26+Brantley%2C+LLP!5e0!3m2!1sen!2sus!4v1528391644893" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-				
-			</div><!-- iframe_map -->
+				<div class="single_contact_list">
 			
-			<div class="contact_info">
+					<div class="iframe_map">
 				
+						<?php the_sub_field( 'iframe' ); ?>
 				
-				<div class="city_title_wrapper">
-					
-					<span class="city_title">Goldsboro</span><!-- city_title -->
-					
-					<a class="city_tel" href="tel:(800) 525-7111">(800) 525-7111</a>
-					
-					
-				</div><!-- city_title_wrapper -->
+					</div><!-- iframe_map -->
+			
+					<div class="contact_info">
 				
-				<div class="city_address_wrapper">
+					<div class="city_title_wrapper">
 					
-					<span class="city_address">601 North Spence Avenue<br/>
-Goldsboro, NC 27534</span><!-- city_address -->
+						<span class="city_title"><?php the_sub_field( 'city_title' ); ?></span><!-- city_title -->
+					
+						<a class="city_tel" href="tel:<?php the_sub_field( 'city_phone_number' ); ?>"><?php the_sub_field( 'city_phone_number' ); ?></a>
+					
+					
+					</div><!-- city_title_wrapper -->
+				
+					<div class="city_address_wrapper">
+					
+						<span class="city_address"><?php the_sub_field( 'city_address' ); ?></span><!-- city_address -->
 
-					<a class="city_directions" href="" target="_blank">Get Directions</a><!-- city_directions -->
+						<a class="city_directions" href="<?php the_sub_field( 'get_directions_google_url' ); ?>" target="_blank">Get Directions</a><!-- city_directions -->
 					
-				</div><!-- city_address_wrapper -->
+					</div><!-- city_address_wrapper -->
 	
-				
-			</div><!-- contact_info -->
+				</div><!-- contact_info -->
 			 
+			</div><!-- single_contact_list -->
+		 
+				
+		    
+			<?php endwhile; ?>
+		 
+		<?php endif; ?>
 		
-		</div><!-- single_contact_list -->
-		
-		<div class="single_contact_list">
-			
-			
-			<div class="iframe_map">
-				
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3253.0610585077893!2d-77.94924298474942!3d35.37894158026594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89ac01ebc7c80817%3A0x744230c84f6e2c87!2sRiddle+%26+Brantley%2C+LLP!5e0!3m2!1sen!2sus!4v1528391644893" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-				
-			</div><!-- iframe_map -->
-			
-			<div class="contact_info">
-				
-				
-				<div class="city_title_wrapper">
-					
-					<span class="city_title">Goldsboro</span><!-- city_title -->
-					
-					<a class="city_tel" href="tel:(800) 525-7111">(800) 525-7111</a>
-					
-					
-				</div><!-- city_title_wrapper -->
-				
-				<div class="city_address_wrapper">
-					
-					<span class="city_address">601 North Spence Avenue<br/>
-Goldsboro, NC 27534</span><!-- city_address -->
-
-					<a class="city_directions" href="" target="_blank">Get Directions</a><!-- city_directions -->
-					
-				</div><!-- city_address_wrapper -->
 	
-				
-			</div><!-- contact_info -->
-			 
-		
-		</div><!-- single_contact_list -->
-		
-		<div class="single_contact_list">
-			
-			
-			<div class="iframe_map">
-				
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3253.0610585077893!2d-77.94924298474942!3d35.37894158026594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89ac01ebc7c80817%3A0x744230c84f6e2c87!2sRiddle+%26+Brantley%2C+LLP!5e0!3m2!1sen!2sus!4v1528391644893" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-				
-			</div><!-- iframe_map -->
-			
-			<div class="contact_info">
-				
-				
-				<div class="city_title_wrapper">
-					
-					<span class="city_title">Goldsboro</span><!-- city_title -->
-					
-					<a class="city_tel" href="tel:(800) 525-7111">(800) 525-7111</a>
-					
-					
-				</div><!-- city_title_wrapper -->
-				
-				<div class="city_address_wrapper">
-					
-					<span class="city_address">601 North Spence Avenue<br/>
-Goldsboro, NC 27534</span><!-- city_address -->
-
-					<a class="city_directions" href="" target="_blank">Get Directions</a><!-- city_directions -->
-					
-				</div><!-- city_address_wrapper -->
-	
-				
-			</div><!-- contact_info -->
-			 
-		
-		</div><!-- single_contact_list -->
-		
-		<div class="single_contact_list">
-			
-			
-			<div class="iframe_map">
-				
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3253.0610585077893!2d-77.94924298474942!3d35.37894158026594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89ac01ebc7c80817%3A0x744230c84f6e2c87!2sRiddle+%26+Brantley%2C+LLP!5e0!3m2!1sen!2sus!4v1528391644893" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-				
-			</div><!-- iframe_map -->
-			
-			<div class="contact_info">
-				
-				
-				<div class="city_title_wrapper">
-					
-					<span class="city_title">Goldsboro</span><!-- city_title -->
-					
-					<a class="city_tel" href="tel:(800) 525-7111">(800) 525-7111</a>
-					
-					
-				</div><!-- city_title_wrapper -->
-				
-				<div class="city_address_wrapper">
-					
-					<span class="city_address">601 North Spence Avenue<br/>
-Goldsboro, NC 27534</span><!-- city_address -->
-
-					<a class="city_directions" href="" target="_blank">Get Directions</a><!-- city_directions -->
-					
-				</div><!-- city_address_wrapper -->
-	
-				
-			</div><!-- contact_info -->
-			 
-		
-		</div><!-- single_contact_list -->
-		
-		
 	</div><!-- contact_list_wrapper -->
 	
 	
