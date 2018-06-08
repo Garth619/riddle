e5962ad40180f61247cf9140912a874f6a2da489
Garrett Cullen <Garth619@users.noreply.github.com>
2018-06-07 20:17:19 -0700

    push

--- wp-content/themes/riddle/404.php
+++ wp-content/themes/riddle/404.php
@@ -1,12 +1,27 @@
 <?php get_header(); ?>
 
+<div class="main">
+
+	<div class="not_found_wrapper">
 	
-				
-<h1 class="entry-title">Not Found</h1>
-				
-				
-<p>Apologies, but the page you requested could not be found.</p>
-				
-<?php // get_sidebar(); ?>
+		<span class="not_found_header">404</span><!-- not_found_header -->
+	
+		<span class="not_found_small_header">Page Not Found</span><!-- not_found_small_header -->
+	
+		<span class="not_found_description">The page you were looking for appears to have been moved, deleted or does not exist. You could <span class="go_back" onclick="goBack()">go back</span> to where you were or head straight to our <a href="<?php bloginfo('url')?>">home page</a>. </span><!-- not_found_description -->
+	
+	</div><!-- not_found_wrapper -->
+
+</div><!-- main -->
+
+
+<script type="text/javascript">
+
+function goBack() {
+    window.history.back();
+}
+
+</script>
+
 			
 <?php get_footer(); ?>
