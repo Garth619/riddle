de1ac6151c49c5c3af5d0e34c197e1c22a54f6a8
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-11 14:28:21 -0700

    push

--- wp-content/themes/riddle/homepage_template_parts/section-4.php
+++ wp-content/themes/riddle/homepage_template_parts/section-4.php
@@ -1,5 +1,189 @@
 <section id="section_four">
 	
-	
+	<div class="sec_four_inner">
+		
+		<div class="sec_four_single">
+			
+			<div class="sec_four_single_inner">
+			
+			<div class="cr_diamond"><?php echo file_get_contents("wp-content/themes/riddle/images/new_diamonds.svg"); ?></div><!-- cr_diamond -->
+			
+			<div class="cr_svg"><?php echo file_get_contents("wp-content/themes/riddle/images/new_cr.svg"); ?></div><!-- cr_svg -->
+			
+			<span class="cr_title">Settlement</span><!-- cr_title -->
+			
+		
+			<span class="cr_amount">$7.85m</span><!-- cr_amount -->
+			
+			
+			<div class="accent">
+				
+				<span class="line"></span><!-- line -->
+				
+				<div class="accent_diamond">
+					
+					<?php echo file_get_contents("wp-content/themes/riddle/images/new_crdiamond.svg"); ?>
+					
+				</div><!-- accent_diamond -->
+				
+			</div><!-- accent -->
+			
+			<span class="cr_description">Class action lawsuit for evacuation damages incurred by residents of the Town of Apex following an explosion at a waste management facility.</span><!-- cr_description -->
+			
+			<a class="consult_button" href="">
+			
+			<div class="consult_left">View All Results</div><!-- consult_left -->
+			
+			<div class="consult_right">
+				
+				<?php echo file_get_contents("wp-content/themes/riddle/images/new_arrow.svg"); ?>
+				
+			</div><!-- consult_right -->
+			
+		</a><!-- consult_button -->
+		
+		</div><!-- sec_four_single_inner -->
+			
+		</div><!-- sec_four_single -->
+		
+		<div class="sec_four_single">
+			
+			<div class="sec_four_single_inner">
+			
+			<div class="cr_diamond"><?php echo file_get_contents("wp-content/themes/riddle/images/new_diamonds.svg"); ?></div><!-- cr_diamond -->
+			
+			<div class="cr_svg"><?php echo file_get_contents("wp-content/themes/riddle/images/new_cr.svg"); ?></div><!-- cr_svg -->
+			
+			<span class="cr_title">Settlement</span><!-- cr_title -->
+			
+		
+			<span class="cr_amount">$7.85m</span><!-- cr_amount -->
+			
+			
+			<div class="accent">
+				
+				<span class="line"></span><!-- line -->
+				
+				<div class="accent_diamond">
+					
+					<?php echo file_get_contents("wp-content/themes/riddle/images/new_crdiamond.svg"); ?>
+					
+				</div><!-- accent_diamond -->
+				
+			</div><!-- accent -->
+			
+			<span class="cr_description">Class action lawsuit for evacuation damages incurred by residents of the Town of Apex following an explosion at a waste management facility.</span><!-- cr_description -->
+			
+			<a class="consult_button" href="">
+			
+			<div class="consult_left">View All Results</div><!-- consult_left -->
+			
+			<div class="consult_right">
+				
+				<?php echo file_get_contents("wp-content/themes/riddle/images/new_arrow.svg"); ?>
+				
+			</div><!-- consult_right -->
+			
+		</a><!-- consult_button -->
+		
+		</div><!-- sec_four_single_inner -->
+			
+		</div><!-- sec_four_single -->
+
+
+		<div class="sec_four_single">
+			
+			<div class="sec_four_single_inner">
+			
+			<div class="cr_diamond"><?php echo file_get_contents("wp-content/themes/riddle/images/new_diamonds.svg"); ?></div><!-- cr_diamond -->
+			
+			<div class="cr_svg"><?php echo file_get_contents("wp-content/themes/riddle/images/new_cr.svg"); ?></div><!-- cr_svg -->
+			
+			<span class="cr_title">Settlement</span><!-- cr_title -->
+			
+		
+			<span class="cr_amount">$7.85m</span><!-- cr_amount -->
+			
+			
+			<div class="accent">
+				
+				<span class="line"></span><!-- line -->
+				
+				<div class="accent_diamond">
+					
+					<?php echo file_get_contents("wp-content/themes/riddle/images/new_crdiamond.svg"); ?>
+					
+				</div><!-- accent_diamond -->
+				
+			</div><!-- accent -->
+			
+			<span class="cr_description">Class action lawsuit for evacuation damages incurred by residents of the Town of Apex following an explosion at a waste management facility.</span><!-- cr_description -->
+			
+			<a class="consult_button" href="">
+			
+			<div class="consult_left">View All Results</div><!-- consult_left -->
+			
+			<div class="consult_right">
+				
+				<?php echo file_get_contents("wp-content/themes/riddle/images/new_arrow.svg"); ?>
+				
+			</div><!-- consult_right -->
+			
+		</a><!-- consult_button -->
+		
+		</div><!-- sec_four_single_inner -->
+			
+		</div><!-- sec_four_single -->
+
+
+		<div class="sec_four_single">
+			
+			<div class="sec_four_single_inner">
+			
+			<div class="cr_diamond"><?php echo file_get_contents("wp-content/themes/riddle/images/new_diamonds.svg"); ?></div><!-- cr_diamond -->
+			
+			<div class="cr_svg"><?php echo file_get_contents("wp-content/themes/riddle/images/new_cr.svg"); ?></div><!-- cr_svg -->
+			
+			<span class="cr_title">Settlement</span><!-- cr_title -->
+			
+		
+			<span class="cr_amount">$7.85m</span><!-- cr_amount -->
+			
+			
+			<div class="accent">
+				
+				<span class="line"></span><!-- line -->
+				
+				<div class="accent_diamond">
+					
+					<?php echo file_get_contents("wp-content/themes/riddle/images/new_crdiamond.svg"); ?>
+					
+				</div><!-- accent_diamond -->
+				
+			</div><!-- accent -->
+			
+			<span class="cr_description">Class action lawsuit for evacuation damages incurred by residents of the Town of Apex following an explosion at a waste management facility.</span><!-- cr_description -->
+			
+			<a class="consult_button" href="">
+			
+			<div class="consult_left">View All Results</div><!-- consult_left -->
+			
+			<div class="consult_right">
+				
+				<?php echo file_get_contents("wp-content/themes/riddle/images/new_arrow.svg"); ?>
+				
+			</div><!-- consult_right -->
+			
+		</a><!-- consult_button -->
+		
+		</div><!-- sec_four_single_inner -->
+			
+		</div><!-- sec_four_single -->
+
+
+		
+				
+			
+	</div><!-- sec_four_inner -->
 	
 </section><!-- section_four -->
