0f1b0bbba2e74e60306d2009d89518c45fcb89d1
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-12 15:26:45 -0700

    push

---
+++ wp-content/themes/riddle/page-about.php
@@ -0,0 +1,57 @@
+<?php 
+
+/* Template Name: About */
+
+get_header(); ?>
+
+
+<div class="main">
+	
+	
+	<div class="about_wrapper">
+		
+		
+		<h1>
+			
+			<span class="about_topline">About <span>Us</span></span><!-- about_topline -->
+			
+			<span class="about_bottomline">who we are at Riddle & Brantley</span><!-- about_bottomline -->
+			
+		</h1>
+		
+		
+	</div><!-- about_wrapper -->
+	
+	<section id="about_section">
+		
+		<img src="<?php bloginfo('template_directory');?>/images/intl_about_img_01_desktop.jpg"/>
+		
+		<div class="about_content_one content">
+			
+			<span class="intro">Riddle & Brantley, LLP is a North Carolina personal injury law firm with a focus on providing experienced legal representation for people who are injured in accidents and for families that lost a loved one due to another’s negligence. Equally important to our law firm is our goal to offer caring and reliable customer service to all our clients. This allows us to provide you with effective and knowledgeable representation as well as the comfort and security you need. Continue reading below to learn about the staff and personal injury attorneys who are here to help you.</span><!-- intro -->
+			
+			<h2>Get to Know Our North Carolina Personal Injury Law Firm</h2>
+			
+			<ul>
+				<li> With over 160 years of combined legal experience, our outstanding attorneys work to get you justice. Your injuries should not keep you from enjoying the highest quality of life possible in your situation.</li>
+				<li>Our attorneys and staff are dedicated to reaching a beneficial solution for you and your loved ones. As a result of this dedication and attentive nature with which we serve our clients’ legal needs, we are proud to establish long-standing relationships with our clients.</li>
+				<li>Tam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</li>
+			</ul>
+
+			
+		</div><!-- about_content_one -->
+		
+	</section><!-- about_section -->
+
+	
+</div><!-- main -->
+
+
+
+<?php get_footer(); ?>
+
+
+
+
+
+	
