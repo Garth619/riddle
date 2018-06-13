a32bd2fc0af8d741ae79a56994400f989b0469d7
Garrett Cullen <Garth619@users.noreply.github.com>
2018-06-12 21:24:33 -0700

    push

--- wp-content/themes/riddle/page-about.php
+++ wp-content/themes/riddle/page-about.php
@@ -24,7 +24,11 @@ get_header(); ?>
 	
 	<section id="about_section">
 		
-		<img src="<?php bloginfo('template_directory');?>/images/intl_about_img_01_desktop.jpg"/>
+		<div class="about_img">
+		
+			<img src="<?php bloginfo('template_directory');?>/images/intl_about_img_01_desktop.jpg"/>
+		
+		</div><!-- about_img -->
 		
 		<div class="about_content_one content">
 			
@@ -41,8 +45,106 @@ get_header(); ?>
 			
 		</div><!-- about_content_one -->
 		
-	</section><!-- about_section -->
+		</section><!-- about_section -->
 
+		
+		<section class="about_section_two">
+			
+			<div class="about_content_two content">
+				
+				<h2>What Do I Need to Know About Riddle & Brantley, LLP?</h2>
+
+				<p>Before hiring any North Carolina personal injury law firm, you should be comfortable with the people protecting your best interests. Below, learn about who we are, what we do, why we do it and where we work.</p>
+
+				<h3>What we are.</h3>
+
+				<p>At Riddle & Brantley, LLP we are personal injury attorneys licensed to practice law in the state of North Carolina. We believe that our system of justice should provide equal access to fairness in our courts of law. As a community of legal professionals, we will fight to preserve, promote and protect our clients’ unalienable rights in their pursuit for justice.</p>
+
+				<h3>What we do.</h3>
+
+				<p>We represent the injured. Often, these injured parties or their families do not have a voice or do not know how to handle the consequences of a serious injury or loss of a loved one. We want to be your voice. In the past, we have represented thousands of clients who have suffered just like you. We invite you to review our case results and listen to what our clients say about us in testimonials.</p>
+
+				<h3>Why we do what we do.</h3>
+
+				<p>We represent our clients because we love to assist those who need our help. Many times, our clients are battling the insurance companies or big businesses and do not know where to turn for justice. We are here for you. We have been the successful voice for thousands of past clients, and we want to be your voice too. If we do not think we can help in your situation, then we will tell you honestly. Hopefully, we can offer you another attorney or resource to contact in such a situation.</p>
+
+				<h2>Where is Riddle & Brantley, LLP?</h2>
+
+				<p>Our North Carolina personal injury law firm has offices in Raleigh, Goldsboro, Jacksonville and Kinston. Founding partner Gene Riddle was born and raised in North Carolina. He spent part of his childhood on his grandfather’s farm in Moore and Montgomery counties where he helped raise hogs and chickens and helped grow tobacco, corn and watermelons. The farm taught him the importance of hard work and dedication. It only made logical sense for Gene to start and maintain his law firm in the great state where he has lived since birth.</p>
+
+				<blockquote>We represent many clients who live in other states but who sustained injuries while visiting North Carolina, on our highways, businesses premises or personal properties.</blockquote>
+
+
+				<p>Gene was educated in the public schools where his father was a very well respected superintendent in Moore county for years. His mother was a teacher. Gene began his legal career in 1985 and now has several hard-working attorneys helping clients throughout the state. We represent people all across the state, from Murphy to Manteo. We also represent many clients who live in other states but who sustained injuries while visiting North Carolina, on our highways, businesses premises or personal properties. If you cannot come to us, then we can come to you. We are happy to talk to you by phone or in person to see if we can help. We also believe in giving back to our communities throughout North Carolina. Our attorneys and staff, therefore, often participate in local fundraisers and events. As part of this work, we advocate for safety in North Carolina and nationwide through our Safety Counts videos.</p>
+				
+			</div><!-- about_content_two -->
+			
+			<div class="about_img_two">
+				
+				
+				<img class="img_one" src="<?php bloginfo('template_directory');?>/images/intl_about_img_02_desktop.jpg"/>
+				
+				<img class="img_two" src="<?php bloginfo('template_directory');?>/images/intl_about_img_03_desktop.jpg"/>
+				
+				<div class="img_content content">
+					
+					<h2>We Have Bilingual Staff, for Your Convenience</h2>
+
+					<p>En Español! Riddle & Brantley, LLP is proud to have many bilingual case managers and staff. Our team can help translate for any person who feels more comfortable speaking Spanish. They will happily assist you when you call concerning a new case and throughout your entire claim. It is important to us that language is not a barrier preventing anyone from seeking justice in the legal system.</p>
+
+					<h2>Paralegal and Case Management</h2>
+
+					<p>Each case with our firm will have both a personal injury attorney and a paralegal or case manager. The case manager or paralegal will maintain your file and help the attorney communicate with you regarding your claim. Our case managers and paralegals have experience with all types of cases and injuries – from soft tissue injuries, head and brain injury and serious bodily injuries, to workplace injuries, as well as other types of cases such as excessive force and civil rights issues.</p>
+
+					<p>Some of our case managers have prior industry experience as insurance adjusters. Therefore, they can offer valuable insight into the property damage and personal injury processes. We even have some dedicated and experienced staff members that have been with our firm for more than 20 years.</p>
+
+					<p>As previously mentioned, some of our case managers are also bilingual to assist clients who prefer to speak Spanish. All our case managers and paralegals work directly under your attorney’s supervision.</p>
+					
+				</div><!-- img_content -->
+				
+			</div><!-- about_img_two -->
+			
+			
+		</section><!-- about_section_two -->
+		
+		<section id="about_section_three">
+			
+			<div class="about_image_three">
+				
+				
+				<img src="<?php bloginfo('template_directory');?>/images/intl_about_img_04_desktop.jpg"/>
+				
+				<div class="about_imge_three_side_content">
+					
+					<span class="side_title">Coconut <span>Riddle</span></span><!-- title -->
+					
+					<span class="side_content">Learn more about our proud animal ambassador and the humane society we support.</span><!-- side_content -->
+					
+					
+					<a class="view_story" href="">View Full Story</a>
+					
+				</div><!-- about_imge_three_side_content -->
+				
+			</div><!-- about_image_three -->
+			
+			<div class="about_content_three content">
+				
+				<h2>Get to Know Our Personal Injury Attorneys</h2>
+
+				<p>Meet the Team! Our personal injury lawyers will fight for you to ensure you get the compensation you deserve after an accident. Meanwhile, they will do everything possible to make your experience in the legal system understandable and comfortable. Riddle & Brantley, LLP currently employs 11 attorneys with over 160 years combined legal experience. Two of our attorneys are Board Certified in Workers Compensation. Additionally, several of our attorneys belong to the Million Dollar Advocates forum and the Multi-Million Dollar Advocates Forum. Many are also ranked among North Carolina Super Lawyers and the Top Trial Lawyers of North Carolina. These organizations honor only small groups of attorneys who have met all membership criteria. Our team often works collectively to help maximize our legal knowledge and experience when we work on your case. We are available to offer you help whenever you need it, 24 hours a day, 7 days a week.</p>
+
+				<h2>Riddle & Brantley, LLP Works with a Team of Investigators</h2>
+
+				<p>Meet our investigators! We have several investigators who help gather information about accidents and injuries for our attorneys. Our investigators also meet with clients who are unable to come to our offices to see with an attorney. They can meet you at your home, office, job site, hospital or any other place most convenient for you. They will provide you with all necessary paperwork the attorney will need to begin working on your case. If you have questions or concerns during their visit, they will call an attorney on the phone to help you.</p>
+					
+				<p>Most of our investigators have prior experience with the North Carolina State Highway Patrol or other law enforcement agencies. We even have a retired District Commander for the North Carolina Highway Patrol who helps communicate with responding officers, OSHA or any other investigatory agency, and witnesses when an incident needs investigating or evidence preservation.</p>
+
+
+
+				
+			</div><!-- about_content_three -->
+			
+		</section><!-- about_section_three -->
 	
 </div><!-- main -->
 
