ebad6d0d5f9202a14fbe21155ed2fef7a873cc25
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-20 11:00:24 -0700

    push

--- wp-content/themes/riddle/homepage_template_parts/section-2.php
+++ wp-content/themes/riddle/homepage_template_parts/section-2.php
@@ -19,7 +19,7 @@
 				<?php while(has_sub_field('selling_points_sec_two')): ?>
 				
 				
-					<div class="sec_two_single_slide">
+					<div class="sec_two_single_slide <?php the_sub_field( 'slide_class' ); ?>">
 				
 				
 						<div class="sp_wrapper">
@@ -32,7 +32,22 @@
 							<span class="slide_title"><?php the_sub_field( 'selling_points_verbiage' ); ?></span><!-- slide_title -->
 				
 							<a class="view_button" href="<?php the_sub_field( 'slider_page_links' ); ?>"><?php the_sub_field( 'button_verbiage' ); ?></a><!-- view_button -->
-						
+							
+							<div class="tool_tip">
+								
+								<div class="tool_tip_inner">
+								
+									<span>Prior results do not guarantee a similar outcome, and they should not be construed to constitute a promise or guarantee of a particular result in any particular case.</span>
+									
+									<div class="tri">
+										
+										<div class="tri_inner"></div><!-- tri_inner -->
+										
+									</div><!-- #tri -->
+								
+								</div><!-- tool_tip_inner -->
+								
+							</div><!-- tool_tip -->
 					
 						</div><!-- sp_wrapper -->
 				
