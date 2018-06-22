db377c98677a1912a31062f488d5b5292b8fd960
Garrett Cullen <garrettwcullen@gmail.com>
2018-06-22 10:41:35 -0700

    push

--- wp-content/themes/riddle/page-about.php
+++ wp-content/themes/riddle/page-about.php
@@ -13,9 +13,9 @@ get_header(); ?>
 		
 		<h1>
 			
-			<span class="about_topline">About <span>Us</span></span><!-- about_topline -->
+			<span class="about_topline"><?php the_field( 'page_title_one' ); ?> <span><?php the_field( 'page_title_two' ); ?></span></span><!-- about_topline -->
 			
-			<span class="about_bottomline">who we are at Riddle & Brantley</span><!-- about_bottomline -->
+			<span class="about_bottomline"><?php the_field( 'page_subtitle' ); ?></span><!-- about_bottomline -->
 			
 		</h1>
 		
@@ -34,13 +34,9 @@ get_header(); ?>
 		
 		<div class="about_content_one content">
 			
-			<span class="intro">Riddle & Brantley, LLP is a North Carolina personal injury law firm with a focus on providing experienced legal representation for people who are injured in accidents and for families that lost a loved one due to another’s negligence. Equally important to our law firm is our goal to offer caring and reliable customer service to all our clients. This allows us to provide you with effective and knowledgeable representation as well as the comfort and security you need. Continue reading below to learn about the staff and personal injury attorneys who are here to help you.</span><!-- intro -->
+			<span class="intro"><?php the_field( 'about_intro_one' ); ?></span><!-- intro -->
 			
-			<h2>Get to Know Our North Carolina Personal Injury Law Firm</h2>
-			
-			<ul>
-				<li> With over 160 years of combined legal experience, our outstanding attorneys work to get you justice. Your injuries should not keep you from enjoying the highest quality of life possible in your situation.</li>
-				<li>Our attorneys and staff are dedicated to reaching a beneficial solution for you and your loved ones. As a result of this dedication and attentive nature with which we serve our clients’ legal needs, we are proud to establish long-standing relationships with our clients.</li>
+			<?php the_field( 'about_image_content' ); ?>
 				
 			</ul>
 
@@ -54,30 +50,7 @@ get_header(); ?>
 			
 			<div class="about_content_two content">
 				
-				<h2>What Do I Need to Know About Riddle & Brantley, LLP?</h2>
-
-				<p>Before hiring any North Carolina personal injury law firm, you should be comfortable with the people protecting your best interests. Below, learn about who we are, what we do, why we do it and where we work.</p>
-
-				<h3>What we are.</h3>
-
-				<p>At Riddle & Brantley, LLP we are personal injury attorneys licensed to practice law in the state of North Carolina. We believe that our system of justice should provide equal access to fairness in our courts of law. As a community of legal professionals, we will fight to preserve, promote and protect our clients’ unalienable rights in their pursuit for justice.</p>
-
-				<h3>What we do.</h3>
-
-				<p>We represent the injured. Often, these injured parties or their families do not have a voice or do not know how to handle the consequences of a serious injury or loss of a loved one. We want to be your voice. In the past, we have represented thousands of clients who have suffered just like you. We invite you to review our case results and listen to what our clients say about us in testimonials.</p>
-
-				<h3>Why we do what we do.</h3>
-
-				<p>We represent our clients because we love to assist those who need our help. Many times, our clients are battling the insurance companies or big businesses and do not know where to turn for justice. We are here for you. We have been the successful voice for thousands of past clients, and we want to be your voice too. If we do not think we can help in your situation, then we will tell you honestly. Hopefully, we can offer you another attorney or resource to contact in such a situation.</p>
-
-				<h2>Where is Riddle & Brantley, LLP?</h2>
-
-				<p>Our North Carolina personal injury law firm has offices in Raleigh, Goldsboro, Jacksonville and Kinston. Founding partner Gene Riddle was born and raised in North Carolina. He spent part of his childhood on his grandfather’s farm in Moore and Montgomery counties where he helped raise hogs and chickens and helped grow tobacco, corn and watermelons. The farm taught him the importance of hard work and dedication. It only made logical sense for Gene to start and maintain his law firm in the great state where he has lived since birth.</p>
-
-				<blockquote>We represent many clients who live in other states but who sustained injuries while visiting North Carolina, on our highways, businesses premises or personal properties.</blockquote>
-
-
-				<p>Gene was educated in the public schools where his father was a very well respected superintendent in Moore county for years. His mother was a teacher. Gene began his legal career in 1985 and now has several hard-working attorneys helping clients throughout the state. We represent people all across the state, from Murphy to Manteo. We also represent many clients who live in other states but who sustained injuries while visiting North Carolina, on our highways, businesses premises or personal properties. If you cannot come to us, then we can come to you. We are happy to talk to you by phone or in person to see if we can help. We also believe in giving back to our communities throughout North Carolina. Our attorneys and staff, therefore, often participate in local fundraisers and events. As part of this work, we advocate for safety in North Carolina and nationwide through our Safety Counts videos.</p>
+				<?php the_field( 'about_content_two' ); ?>
 				
 			</div><!-- about_content_two -->
 			
@@ -92,17 +65,7 @@ get_header(); ?>
 				
 				<div class="img_content content">
 					
-					<h2>We Have Bilingual Staff, for Your Convenience</h2>
-
-					<p>En Español! Riddle & Brantley, LLP is proud to have many bilingual case managers and staff. Our team can help translate for any person who feels more comfortable speaking Spanish. They will happily assist you when you call concerning a new case and throughout your entire claim. It is important to us that language is not a barrier preventing anyone from seeking justice in the legal system.</p>
-
-					<h2>Paralegal and Case Management</h2>
-
-					<p>Each case with our firm will have both a personal injury attorney and a paralegal or case manager. The case manager or paralegal will maintain your file and help the attorney communicate with you regarding your claim. Our case managers and paralegals have experience with all types of cases and injuries – from soft tissue injuries, head and brain injury and serious bodily injuries, to workplace injuries, as well as other types of cases such as excessive force and civil rights issues.</p>
-
-					<p>Some of our case managers have prior industry experience as insurance adjusters. Therefore, they can offer valuable insight into the property damage and personal injury processes. We even have some dedicated and experienced staff members that have been with our firm for more than 20 years.</p>
-
-					<p>As previously mentioned, some of our case managers are also bilingual to assist clients who prefer to speak Spanish. All our case managers and paralegals work directly under your attorney’s supervision.</p>
+					<?php the_field( 'about_content_two_second_column' ); ?>
 					
 				</div><!-- img_content -->
 				
@@ -120,12 +83,12 @@ get_header(); ?>
 				
 				<div class="about_imge_three_side_content">
 					
-					<span class="side_title">Coconut <span>Riddle</span></span><!-- title -->
+					<span class="side_title"><?php the_field( 'coconut_riddle_title' ); ?></span><!-- title -->
 					
-					<span class="side_content">Learn more about our proud animal ambassador and the humane society we support.</span><!-- side_content -->
+					<span class="side_content"><?php the_field( 'coconut_riddle_excerpt' ); ?></span><!-- side_content -->
 					
 					
-					<a class="view_story" href="<?php bloginfo();?>/who-we-are/coconut-riddle/">View Full Story</a>
+					<a class="view_story" href="<?php the_field( 'coconut_button_link' ); ?>"><?php the_field( 'coconut_button_verbiage' ); ?></a>
 					
 				</div><!-- about_imge_three_side_content -->
 				
@@ -133,19 +96,8 @@ get_header(); ?>
 			
 			<div class="about_content_three content">
 				
-				<h2>Get to Know Our Personal Injury Attorneys</h2>
+				<?php the_field( 'about_content_three' ); ?>
 
-				<p>Meet the Team! Our personal injury lawyers will fight for you to ensure you get the compensation you deserve after an accident. Meanwhile, they will do everything possible to make your experience in the legal system understandable and comfortable. Riddle & Brantley, LLP currently employs 11 attorneys with over 160 years combined legal experience. Two of our attorneys are Board Certified in Workers Compensation. Additionally, several of our attorneys belong to the Million Dollar Advocates forum and the Multi-Million Dollar Advocates Forum. Many are also ranked among North Carolina Super Lawyers and the Top Trial Lawyers of North Carolina. These organizations honor only small groups of attorneys who have met all membership criteria. Our team often works collectively to help maximize our legal knowledge and experience when we work on your case. We are available to offer you help whenever you need it, 24 hours a day, 7 days a week.</p>
-
-				<h2>Riddle & Brantley, LLP Works with a Team of Investigators</h2>
-
-				<p>Meet our investigators! We have several investigators who help gather information about accidents and injuries for our attorneys. Our investigators also meet with clients who are unable to come to our offices to see with an attorney. They can meet you at your home, office, job site, hospital or any other place most convenient for you. They will provide you with all necessary paperwork the attorney will need to begin working on your case. If you have questions or concerns during their visit, they will call an attorney on the phone to help you.</p>
-					
-				<p>Most of our investigators have prior experience with the North Carolina State Highway Patrol or other law enforcement agencies. We even have a retired District Commander for the North Carolina Highway Patrol who helps communicate with responding officers, OSHA or any other investigatory agency, and witnesses when an incident needs investigating or evidence preservation.</p>
-
-
-
-				
 			</div><!-- about_content_three -->
 			
 		</section><!-- about_section_three -->
@@ -153,9 +105,9 @@ get_header(); ?>
 		<section id="about_section_four">
 			
 			
-			<span class="request_title">Requesting a Consultation</span><!-- request_title -->
+			<span class="request_title"><?php the_field( 'about_request_title' ); ?></span><!-- request_title -->
 			
-			<span class="request_description">When you call Riddle & Brantley, LLP concerning a potential case, you will speak with a member of our intake staff who will help gather information about your case for an attorney to review. At any time, you may request to speak with an attorney, and one will contact you as soon as possible. All of our intake staff work under attorney supervision. If you need legal assistance or have questions about an accident, then contact our North Carolina personal injury law firm today to schedule a free initial consultation at any of our local offices.</span><!-- request_description -->
+			<span class="request_description"><?php the_field( 'about_request_content' ); ?></span><!-- request_description -->
 			
 			
 			<a class="consult_button desktop" href="#consultation">
--- wp-content/themes/riddle/page-about_espanol.php
+++ wp-content/themes/riddle/page-about_espanol.php
@@ -15,7 +15,7 @@ get_header(); ?>
 			
 			<span class="about_topline">Inicio</span><!-- about_topline -->
 			
-			<span class="about_bottomline">Riddle & Brantley</span><!-- about_bottomline -->
+			<span class="about_bottomline"><?php the_field( 'page_subtitle_espanol' ); ?></span><!-- about_bottomline -->
 			
 		</h1>
 		
@@ -34,26 +34,9 @@ get_header(); ?>
 		
 		<div class="about_content_one content">
 			
-			<span class="intro">Nuestra firma representa personas lesionadas debido a la negligencia de otros en todo tipo de accidentes. También ayudamos a personas que han sido heridas en el trabajo y tenemos un departamento para personas solicitando seguro social por incapacidad y beneficios para veteranos. Tome un momento para explorar nuestro sitio en línea, conózcanos y aprenda como les podemos ayudar. Por favor mándenos un email o llámenos si quiere más información o si quiere discutir su caso.</span><!-- intro -->
+			<span class="intro"><?php the_field( 'about_intro_one_espanol' ); ?></span><!-- intro -->
 			
-			<h2>Bienvenidos a la página de internet de nuestra firma.</h2>
-
-			<p>Disfrute nuestro sitio en línea y aquí está una lista corta de nuestras áreas de práctica destacadas (mire todas):</p>
-			
-			<ul class="columns">
-				<li>Accidentes de Auto</li>
-				<li>Accidentes Automovilísticos</li>
-				<li>Lesiones Personales Serias</li>
-				<li>Drogas Defectuosas o<br/> Dispositivos Médicos Peligrosos</li>
-				<li>Accidentes de Camión/Tráiler</li>
-				<li>Accidentes de Peatón o Ciclista</li>
-				<li>Muerte Injusta</li>
-				<li>Resbalón y Caída/ Responsabilidad<br/> de Premisas</li>
-				<li>Compensación Para Trabajadores</li>
-				<li>Seguro Social por Incapacidad</li>
-				<li>Beneficios de Veteranos</li>
-				<li>Ataques de Animal y Mordeduras de Perro</li>
-			</ul>
+			<?php the_field( 'about_image_content_espanol' ); ?>
 
 			
 		</div><!-- about_content_one -->
@@ -65,58 +48,7 @@ get_header(); ?>
 			
 			<div class="about_content_two content">
 				
-				<h2>Abogados de Lesiones Personales de Carolina del Norte</h2>
-
-				<p>Lesiones por accidentes que fueron causados por el descuido de otros pueden tener efectos devastadores para una familia entera. En el bufete de Riddle & Brantley, LLP, usamos nuestra experiencia legal combinada de más de 160 años para trabajar por resultados que pueden aliviar un poco el estrés causado por una herida.</p>
-
-<p>También tenemos recursos extensivos para ayudar con su reclamación. Nuestra firma tiene oficiales de policía retirados de la Carolina del Norte que pueden ayudar con la investigación y reunir evidencia que podría ayudarnos a construir un caso sucesivo. Nuestros abogados de lesiones personales expertos de Carolina del Norte representan clientes con una variedad de asuntos relacionados a lesiones personales:</p>
-
-<ul class="columns">
-	<li>Herida en Sitio de Construcción</li>
-	<li>Drogas y Dispositivos Peligrosos</li>
-	<li>Mordeduras de Perro y Animal</li>
-	<li>Mesotelioma y Asbestos</li>
-	<li>Abuso en Hogar en Ancianos</li>
-	<li>Litigación Farmacéutica</li>
-	<li>Responsabilidad de Premisas</li>
-	<li>Responsabilidad de Productos</li>
-	<li>Heridas de Resbalón y Caída</li>
-	<li>Accidentes Vehiculares</li>
-</ul>
-
-<h2>Accidente Juristas</h2> 
-<p>Nuestros abogados y personales conocen bien los aspectos de la ley y hacemos un gran esfuerzo para nuestros clientes. Nos esforzamos en lograr la mejor solución que es la más adecuada para su situación única.</p>
-
-<p>Nuestro equipo quiere asegurarse de que usted entienda la naturaleza de su caso antes de proceder con cualquier acción. Explicaremos los hechos mientras contestamos sus preguntas honestamente para que usted pueda hacer una decisión informada sobre su caso.
-Los abogados de accidentes automovilísticos en Riddle & Brantley, LLP tienen más de 160 años de experiencia combinada que incluye, pero no está limitado a:</p>
-
-<ul class="columns">
-<li>Accidentes de Peatones y Ciclistas</li>
-<li>Accidentes de Carro</li>
-<li>Accidentes por Conductores Distractos</li>
-<li>Accidentes por Conductores Borrachos</li>
-<li>Accidente Por Frente</li>
-<li>Accidente de Golpe y Fuga</li>
-<li>Accidente Automovilístico</li>
-<li>Accidente de Motocicleta</li>
-<li>Accidente Posterior</li>
-<li>Accidente por Conducción Temeraria</li>
-<li>Accidente Volcadura</li>
-<li>Herida de Cinturón</li>
-<li>Accidente por Exceso Velocidad</li>
-<li>Accidente de Textear Mientras Conduce</li>
-<li>Accidente Camiónero</li>
-</ul>
-
-
-<h2>Abogados de Compensación Para Trabajadores</h2>
-
-<p>¿Sufrió una herida en el trabajo? ¿No puede hacer sus deberes en el trabajo? Si esto suena como usted, entonces podría ser elegible para recibir compensación para trabajadores. Nuestra firma de ley luchara por usted y sus derechos para asegurar de que usted reciba compensación por su caso. Les asistiremos a través del proceso paso-a -paso que incluye: investigando y solicitando beneficios hasta que sean concedidos, apelar la decisión o incluso volver a instaurar su reclamo.</p>
-
-<p>¡El abogado de compensación para trabajadores, Chris Brantley es un especialista certificado en Carolina del Norte en compensación para trabajadores y examina todos los casos!</p>
-
-<p>También hemos reunido una lista de cuestiones frecuentes y usted puede mirar esas cuestiones aquí en nuestro FAQ sobre Compensación de Trabajadores.
-Usted tiene derechos y opciones – ejércelos con uno de nuestros abogados de compensación para trabajadores.</p>
+				<?php the_field( 'about_content_two_espanol' ); ?>
 				
 			</div><!-- about_content_two -->
 			
@@ -131,26 +63,7 @@ Usted tiene derechos y opciones – ejércelos con uno de nuestros abogados de c
 				
 				<div class="img_content content">
 					
-					<h2>Abogados de Seguro Social por Incapacidad</h2>
-
-					<p>Si usted o un ser querido está luchando para recibir beneficios de seguro social por incapacidad, nuestros abogados le pueden ayudar a ejercer sus derechos y explorar sus opciones legales. Para poder calificar para seguro social por incapacidad un individuo tiene que tener una condición médica que cumpla con la definición de incapacidad de la Oficina de Seguro Social.</p>
-					
-					<p>Los abogados de incapacidad de Riddle & Brantley, LLP conocen el sistema de seguro social y pueden luchar por los beneficios que merece.
-Tal vez encuentre nuestra lista de preguntas frecuentes sobre el seguro social por incapacidad útil:</p>
-
-	<ul class="columns">
-		<li>¿Qué es Seguro Social por Incapacidad?</li>
-		<li>¿Qué es Una Incapacidad?</li>
-		<li>¿Cómo es Determinada la Incapacidad?</li>
-		<li>¿Cuánto Tiempo se Demora en Tomar Una Decisión?</li>
-		<li>¿Puedo Recibir Compensación de Trabajador o Seguro Social por Incapacidad a la misma vez?</li>
-	</ul>
-	
-	<h2>Juristas de Beneficios Para Veteranos</h2>
-
-<p>¡En Riddle & Brantley, LLP, cada uno de nuestros abogados tiene miembros de familia que han servido en las fuerzas armadas, y creemos que veteranos incapacitados deben ser intitulados a recibir los beneficios que se merecen!</p>
-
-<p>Nuestro equipo trabajará sin descanso a su nombre, para lograr los beneficios máximo que usted merece. Con ubicaciones de oficinas en Goldsboro, Raleigh, Kinston y Jacksonville, Carolina del Norte, nuestros abogados pueden servir en todo el estado, y en otros lugares necesarios.¡Para más información, les invitamos a llamarnos al <a href="tel:800-525-7111">800-525-7111</a>, o complete el formulario en esta pagina para una Evaluación de Caso Gratuita!</p>
+					<?php the_field( 'about_content_two_second_column_espanol' ); ?>
 					
 				</div><!-- img_content -->
 				
@@ -171,6 +84,9 @@ Tal vez encuentre nuestra lista de preguntas frecuentes sobre el seguro social p
 			
 			<div class="about_section_es_content content">
 				
+				<?php the_field( 'about_section_four_espanol' ); ?>
+				
+<!--
 				<h2>Contacte los Abogados de Lesiones en Riddle & Brantley, LLP</h2>
 
 				<p>Entendemos que unas víctimas no pueden desplazarse después de ser lesionado debido a la negligencia de otros, y es por eso que las oficinas de Riddle & Brantley, LLP están orgullosos de hacer visitas de hospital u hogar.</p>
@@ -204,7 +120,7 @@ Tal vez encuentre nuestra lista de preguntas frecuentes sobre el seguro social p
 							
 						</ul>
 						
-					</div><!-- four_col -->
+					</div>
 					
 					<div class="four_col">
 						
@@ -222,7 +138,7 @@ Tal vez encuentre nuestra lista de preguntas frecuentes sobre el seguro social p
 							
 						</ul>
 						
-					</div><!-- four_col -->
+					</div>
 					
 					<div class="four_col">
 						
@@ -240,7 +156,7 @@ Tal vez encuentre nuestra lista de preguntas frecuentes sobre el seguro social p
 							
 						</ul>
 						
-					</div><!-- four_col -->
+					</div>
 					
 					
 					<div class="four_col">
@@ -256,11 +172,12 @@ Tal vez encuentre nuestra lista de preguntas frecuentes sobre el seguro social p
 							<li>y las áreas alrededort</li>			
 						</ul>
 						
-					</div><!-- four_col -->
+					</div>
 					
 					
 					
-				</div><!-- four_columns -->
+				</div>
+-->
 				
 				
 			</div><!-- about_section_es_content -->
