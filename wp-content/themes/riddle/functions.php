ebafcbfa047e24443c7deb41b573b33091109b9c
Garrett Cullen <garrettwcullen@gmail.com>
2018-07-09 15:39:30 -0700

    push

--- wp-content/themes/riddle/functions.php
+++ wp-content/themes/riddle/functions.php
@@ -227,13 +227,16 @@ add_filter('upload_mimes', 'cc_mime_types');
 
 
 
-function is_tree($pid) {      // $pid = The ID of the page we're looking for pages underneath
-	global $post;         // load details about this page
+/* 
+	// parent child sibling pages
+function is_tree($pid) {      
+	global $post;         
 	if(is_page()&&($post->post_parent==$pid||is_page($pid))) 
-               return true;   // we're at the page or at a sub page
+               return true;   
 	else 
-               return false;  // we're elsewhere
+               return false;  
 };
+*/
 
 
 
